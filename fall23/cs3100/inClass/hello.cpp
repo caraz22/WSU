03836a40d181ecec4047292396124082a5491b3f
caraz22 <cszoke22@gmail.com>
2023-08-31 18:03:28 -0400

    in class work

---
+++ fall23/cs3100/inClass/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // int i = 7;
+    // cout << "Hello world" << endl;
+    // cout << i << endl;
+    // string response;
+    // cin >> response;
+
+    int myArray[5];
+    for (int i = 0; i < 5; i++) {
+        myArray[i] = i * 10;
+    }
+
+    cout << "Done" << endl;
+}
