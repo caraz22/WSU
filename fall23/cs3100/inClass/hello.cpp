0a0b35e2513578a9a88adc9409fa9a06d2feb559
caraz22 <cszoke22@gmail.com>
2023-09-01 09:30:50 -0400

    got compiler building and running correctly

--- fall23/cs3100/inClass/hello.cpp
+++ fall23/cs3100/inClass/hello.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main() {
     // int i = 7;
-    // cout << "Hello world" << endl;
+    cout << "Hello world" << endl;
     // cout << i << endl;
     // string response;
     // cin >> response;
 
-    int myArray[5];
-    for (int i = 0; i < 5; i++) {
-        myArray[i] = i * 10;
-    }
+    // int myArray[5];
+    // for (int i = 0; i < 5; i++) {
+    //     myArray[i] = i * 10;
+    // }
 
-    cout << "Done" << endl;
+    // cout << "Done" << endl;
 }
