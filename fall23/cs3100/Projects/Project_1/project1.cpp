1f5b408ffc942400d21e079f1299a5341a15456b
caraz22 <cszoke22@gmail.com>
2023-09-07 18:21:00 -0400

    fixed things according to adrien

---
+++ fall23/cs3100/Projects/Project_1/project1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    // srand(time(NULL));
+    // int randNum;
+
+    cout << "How many rolls? ";
+    int rolls;
+    cin >> rolls;
+    cout << endl << "Simulating " << rolls << " rolls..." << endl;
+
+    
+}
--- fall23/cs3100/inClass/arraySize/arraySize.cpp
+++ fall23/cs3100/inClass/arraySize/arraySize.cpp
@@ -3,29 +3,38 @@
 using namespace std;
 
 int main() {
+    /*
     int arraySize;  // Stack
     double d;   // Stack   
     static int something;   // Statics area
     cout << "How big of an array do you want?" << endl;
     cin >> arraySize;
     cout << endl << "Creating an array of size: " << arraySize << endl;
+    */
 
-    // int myArray[arraySize];
-    int * myArray;
+    int myArray[5] = {1, 2, 3, 4, 5};
+    // int * myArray;
     int * p;
 
-    myArray = new int[arraySize];
-    p = myArray;
+    for (int i = 0; i < 5; i++) {
+        cout << myArray[i] << endl;
+    }
 
-    for (int i = 0; i < arraySize; i++) {
-        myArray[i] = 2 * i;
+    // myArray = new int[5];
+    p = myArray;
+    cout << endl;
+
+    for (int i = 0; i < 5; i++) {
+        *p *= 2;
+        p += 1;
+        // myArray[i] = 2 * i;
     }
 
     // Release myArray
-    delete [] myArray;
+    // delete [] myArray;
 
     // Print out the array
-    for (int i = 0; i < arraySize; i++) {
+    for (int i = 0; i < 5; i++) {
         cout << myArray[i] << endl;
     }
 
