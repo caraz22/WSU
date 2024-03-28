ad5f8d6919ce2123660303bb8adc0fadc9776b44
caraz22 <cszoke22@gmail.com>
2024-03-28 18:34:21 -0400

    project finished

--- spring24/cs3100/Projects/Project_4/AVLTree.cpp
+++ spring24/cs3100/Projects/Project_4/AVLTree.cpp
@@ -1,70 +1,92 @@
 /*
-INCLUDE HEADER
+Cara Zozokos
+Project 4 - AVL Trees
+This is the implementation file for project 4
 */
-#include "AVLTree.h"
+#include "AVLTree.h" // Include header file for AVLTree class
 
+// Constructor: Initializes AVL tree with root as nullptr, size and height as 0
 AVLTree::AVLTree() {
     root = nullptr;
     treeSize = 0;
     treeHeight = 0;
 }
 
+// Copy constructor: Creates a deep copy of the AVL tree
 AVLTree::AVLTree(const AVLTree& tree) {
     root = copyTree(tree.root);
 }
 
+// Recursive function to copy a tree starting from a given node
 AVLTree::TreeNode* AVLTree::copyTree(TreeNode* current) {
-    if (current = nullptr) {
+    if (current == nullptr) {
         return nullptr;
     }
 
+    // Create a new node with the same key and value
     TreeNode* newNode = new TreeNode(current->key, current->value);
+
+    // Recursively copy left and right subtrees
     newNode->left = copyTree(current->left);
     newNode->right = copyTree(current->right);
+
     return newNode;
 }
 
+// Destructor: Deallocates memory by clearing the AVL tree
 AVLTree::~AVLTree() {
     clear(root);
 }
 
+// Recursive function to clear the AVL tree
 void AVLTree::clear(TreeNode* current) {
-    if (root == nullptr) {
+    if (current == nullptr) {
         return;
     }
 
-    while (current != nullptr) {
-        clear(current->left);
-        clear(current->right);
-        delete current;
-    }
+    // Recursively delete left and right subtrees
+    clear(current->left);
+    clear(current->right);
+
+    // Delete the current node
+    delete current;
 }
 
+// Insert a new node with given key and value into the AVL tree
 bool AVLTree::insert(int keyNum, string valueStr) {
     return insertHelper(keyNum, valueStr, root);
 }
 
+// Helper function for insert
 bool AVLTree::insertHelper(int keyNum, string valueStr, TreeNode*& current) {
+    // Base case: If current node is null, insert new node here
     if (current == nullptr) {
         current = new TreeNode(keyNum, valueStr);
         treeSize++;
-        return true;
 
+        // Perform rebalancing if necessary
         int balance = getBalance(current);
         if (balance >= 2) {
             singleRotate(current, current->left);
         } else if (balance <= -2) {
             singleRotate(current, current->right);
         }
-    } else if (keyNum < current->key) {
+
+        return true;
+    }
+    // Recursive cases: Insert into left or right subtree based on key comparison
+    else if (keyNum < current->key) {
         return insertHelper(keyNum, valueStr, current->left);
     } else if (keyNum > current->key) {
         return insertHelper(keyNum, valueStr, current->right);
-    } else if (find(keyNum, valueStr) == true) {
+    }
+    // If key already exists in the tree, return false
+    else {
         return false;
     }
 } 
 
+// Get the balance factor of a node in the AVL tree
 int AVLTree::getBalance(TreeNode* current) {
     if (current == nullptr) {
         return 0;
@@ -75,10 +97,12 @@ int AVLTree::getBalance(TreeNode* current) {
     return leftHeight - rightHeight;
 }
 
+// Get the height of the AVL tree
 int AVLTree::getHeight() {
     return getHeightHelper(root);
 }
 
+// Recursive helper function to get the height of a node in the AVL tree
 int AVLTree::getHeightHelper(TreeNode*& current) {
     if (current == nullptr) {
         return 0;
@@ -87,14 +111,17 @@ int AVLTree::getHeightHelper(TreeNode*& current) {
     return max(getHeightHelper(current->left), getHeightHelper(current->right)) + 1;
 }
 
+// Get the size of the AVL tree
 int AVLTree::getSize() {
     return treeSize;
 }
 
+// Find a node with the given key and store its value in the second parameter
 bool AVLTree::find(int keyNum, string& valueStr) {
     return findHelper(keyNum, valueStr, root);
 }
 
+// Recursive helper function to find a node with the given key
 bool AVLTree::findHelper(int keyNum, string& valueStr, TreeNode*& current) {
     if (current == nullptr) {
         return false;
@@ -103,17 +130,19 @@ bool AVLTree::findHelper(int keyNum, string& valueStr, TreeNode*& current) {
         return true;
     } else if (current->key < keyNum) {
         return findHelper(keyNum, valueStr, current->right);
-    } else if (current->key > keyNum) {
+    } else { // (current->key > keyNum)
         return findHelper(keyNum, valueStr, current->left);
     }
 }
 
+// Find all values within the given range of keys
 vector<string> AVLTree::findRange(int start, int end) {
     vector<string> range;
     findRangeHelper(range, start, end, root);
     return range;
 }
 
+// Recursive helper function to find all values within a given range of keys
 void AVLTree::findRangeHelper(vector<string>& range, int start, int end, TreeNode*& current) {
     if (current == nullptr) {
         return;
@@ -132,6 +161,7 @@ void AVLTree::findRangeHelper(vector<string>& range, int start, int end, TreeNod
     }
 }
 
+// Perform a single rotation to balance the AVL tree
 void AVLTree::singleRotate(TreeNode*& problem, TreeNode*& hook) {
     if (getBalance(problem) > 1) {
         hook = problem;
@@ -142,6 +172,7 @@ void AVLTree::singleRotate(TreeNode*& problem, TreeNode*& hook) {
     }
 }
 
+// Print the AVL tree
 void AVLTree::print(ostream& os, TreeNode* current, string indent) const {   
     if (current == nullptr) {
         return;
@@ -154,6 +185,7 @@ void AVLTree::print(ostream& os, TreeNode* current, string indent) const {
     print (os, current->left, indent);
 }
 
+// Overloaded stream insertion operator for printing the AVL tree
 ostream& operator<<(ostream& os, const AVLTree& tree) {
     string indent = "       ";
     tree.print(os, tree.root, indent);
--- spring24/cs3100/Projects/Project_4/AVLTree.h
+++ spring24/cs3100/Projects/Project_4/AVLTree.h
@@ -1,5 +1,7 @@
 /*
-INCLUDE HEADER
+Cara Zozokos
+Project 4 - AVL Trees
+This is the header file for project 4
 */
 #include <iostream>
 #include <vector>
@@ -8,66 +10,76 @@ INCLUDE HEADER
 using namespace std;
 
 class AVLTree {
-    private:
+private:
+    // TreeNode class represents each node in the AVL tree
     class TreeNode {
-        public:
-        TreeNode* left;
-        TreeNode* right;
-        // TreeNode* hook;
-        // TreeNode* problem;
-        
-        int key;
-        string value;
+    public:
+        TreeNode* left;   // Pointer to the left child
+        TreeNode* right;  // Pointer to the right child
+        int key;          // Key value of the node
+        string value;     // Associated value of the node
 
-        TreeNode() : left(nullptr), right(nullptr) {
-        } 
+        // Constructors
+        TreeNode() : left(nullptr), right(nullptr) {}
 
-        TreeNode(int keyNum, string valueStr) : key(keyNum), value(valueStr), left(nullptr), right(nullptr) {
-        }
+        TreeNode(int keyNum, string valueStr) : key(keyNum), value(valueStr), left(nullptr), right(nullptr) {}
     };
 
-    public:
-    TreeNode* root;   
-    int treeSize; 
-    int treeHeight;
+public:
+    TreeNode* root;     // Pointer to the root node of the AVL tree
+    int treeSize;       // Number of nodes in the AVL tree
+    int treeHeight;     // Height of the AVL tree
 
-    AVLTree();
+    // Constructors and Destructor
+    AVLTree();                           // Default constructor
+    AVLTree(const AVLTree&);             // Copy constructor
+    ~AVLTree();                          // Destructor
 
-    AVLTree(const AVLTree&);
-
-    TreeNode* copyTree(TreeNode*);
-
+    // Copy assignment operator
     AVLTree& operator=(const AVLTree&);
 
-    ~AVLTree();
-
+    // Clear the AVL tree
     void clear(TreeNode*);
 
+    // Insert a new node with given key and value into the AVL tree
     bool insert(int, string);
 
+    // Helper function for insert
     bool insertHelper(int, string, TreeNode*&);
 
+    // Get the height of the AVL tree
     int getHeight();
 
+    // Helper function for getHeight
     int getHeightHelper(TreeNode*&);
 
+    // Get the size of the AVL tree
     int getSize();
 
+    // Overloaded stream insertion operator for printing the AVL tree
     friend ostream& operator<<(ostream&, const AVLTree&);
 
+    // Print the AVL tree
     void print(ostream&, TreeNode*, string) const;
 
+    // Find a node with the given key and store its value in the second parameter
     bool find(int, string&);
 
+    // Helper function for find
     bool findHelper(int, string&, TreeNode*&);
 
+    // Find all values within the given range of keys
     vector<string> findRange(int, int);
 
+    // Helper function for findRange
     void findRangeHelper(vector<string>&, int, int, TreeNode*&);
 
-    void singleRotate(TreeNode*&, TreeNode*&); // week 7 lecture 2
+    // Perform a single rotation to balance the AVL tree
+    void singleRotate(TreeNode*&, TreeNode*&);    
 
-    void doubleRotate(TreeNode*&, TreeNode*&); // week 8 lecture 1
-
+    // Get the balance factor of a node in the AVL tree
     int getBalance(TreeNode*);
+
+    // Copy the AVL tree rooted at the given node and return the new root
+    TreeNode* copyTree(TreeNode*);
 };
