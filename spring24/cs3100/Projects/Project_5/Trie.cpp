279708a003d571917de889535895494a7f07c689
caraz22 <cszoke22@gmail.com>
2024-04-04 23:02:57 -0400

    created TrieNode class, did constructor and started insert

--- spring24/cs3100/Projects/Project_5/Trie.cpp
+++ spring24/cs3100/Projects/Project_5/Trie.cpp
@@ -1,1 +1,34 @@
 #include "Trie.h"
+
+Trie::Trie() {
+    root = new TrieNode();
+    numWords = 0;
+    numNodes = 1;
+}
+
+Trie::Trie(const Trie& trieCopy) {
+
+}
+
+Trie::~Trie() {
+
+}
+
+bool Trie::insert(string word) {
+    if (find(word) == true) {
+        return false;
+    }
+
+    insertHelper(word, root);
+}
+
+bool Trie::insertHelper(string word, TrieNode*& current) {
+    for (char c : word) {
+        if (current->character != c) {
+            current->children[c] = new TrieNode();
+        }
+        current = current->children[c];
+    }
+
+    
+}
--- spring24/cs3100/Projects/Project_5/Trie.h
+++ spring24/cs3100/Projects/Project_5/Trie.h
@@ -5,16 +5,33 @@
 using namespace std;
 
 class Trie {
+    private:
+    class TrieNode {
+        public:
+        char character;
+        vector<TrieNode*> children;
+        bool endOfWord;
+
+        TrieNode() : character(' '), children(26, nullptr), endOfWord(false) {}
+    };
+
     public:
+    TrieNode* root;
+    int numWords;
+    int numNodes;
+
     Trie();
     Trie(const Trie&);
     ~Trie();
     
     bool insert(string);
+    bool insertHelper(string, TrieNode*&);
     int count();
     int getSize();
     bool find(string);
     int completeCount(string);
     vector<string> complete(string);
     Trie& operator=(const Trie&);
+    
+    // num of leaf nodes = num of words
 };
