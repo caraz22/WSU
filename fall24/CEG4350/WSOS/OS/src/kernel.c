2d33d5d650f8abf7e16b6ecc883fb560ec500375
caraz22 <cszoke22@gmail.com>
2024-10-02 10:34:09 -0400

    in class project code

--- fall24/CEG4350/WSOS/OS/src/kernel.c
+++ fall24/CEG4350/WSOS/OS/src/kernel.c
@@ -1,23 +1,54 @@
 #include "./io.h"
+#include "./multitasking.h"
+#include "./irq.h"
+#include "./isr.h"
+
+void prockernel();
+void proca();
 
 int main() 
 {
-	initkeymap();
-	
 	// Clear the screen
 	clearscreen();
 
-	// Print "Hello World!"
-	printf("Hello World!");
+	// Initialize our keyboard
+	initkeymap();
 
-	char input[100];
-
-	while(1) {
-		printf("\nPlease type something: ");
-		scanf(input);
-		printf("\nYou typed: ");
-		printf(input);
-	}
+	startkernel(prockernel);
 	
 	return 0;
+}
+
+void prockernel()
+{
+	printf("Starting Kernel Process...\n");
+	
+	// Create the user processes
+
+	createproc(proca, (void *) 0x10000);
+
+	// Schedule the next process
+
+	int userprocs = schedule();
+
+	while(userprocs > 0)
+	{
+		yield();
+		printf("Resuming Kernel Process\n");
+		userprocs = schedule();
+	}
+
+	printf("Exiting Kernel Process...\n");
+}
+
+// The user processes
+
+void proca()
+{
+	printf("Starting User Process A\n");
+	yield();
+	printf("Resuming User Process A\n");
+	yield();
+	printf("Exiting User Process A\n");
+	exit();
 }
