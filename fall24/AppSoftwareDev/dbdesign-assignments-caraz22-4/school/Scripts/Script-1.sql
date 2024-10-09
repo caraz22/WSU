b63462bf6403ce7120a287385d158411fe850eb6
caraz22 <cszoke22@gmail.com>
2024-10-09 10:58:31 -0400

    app software dev stuff

--- fall24/AppSoftwareDev/dbdesign-assignments-caraz22-4/school/Scripts/Script-1.sql
+++ fall24/AppSoftwareDev/dbdesign-assignments-caraz22-4/school/Scripts/Script-1.sql
@@ -1,8 +1,7 @@
-SELECT * from work_order_pro.technician t;
-
-UPDATE work_order_pro.technician set technician_type='Plumber' where technician_code <= 'T10490';
-UPDATE work_order_pro.technician set technician_type='Electrician' where technician_code > 'T10490';
-
-#ALTER table work_order_pro.technician add column technician_type varchar(15)
-
-SELECT * from work_order_pro.state s;
+Select t.technician_code as code,t.technician_first_name as firstName,t.technician_last_name as lastName,
+wos.work_order_status_desc as workOrderStatus, t.technician_type as type, GROUP_CONCAT(twp.state_code SEPARATOR ',') 
+as states  from work_order_pro.technician t left join work_order_pro.technician_work_permit twp
+on twp.technician_code =t.technician_code left join work_order_pro.work_order wo on wo.work_order_number =
+(Select max(wo1.work_order_number) from work_order_pro.work_order wo1 where wo1.technician_code = t.technician_code)
+left join work_order_pro.work_order_status wos on wos.work_order_status_code = wo.work_order_status_code
+where t.technician_code = 'T10481' group by t.technician_code;
