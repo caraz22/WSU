471962b22db976e04c96e33d25ab88e01766d854
caraz22 <cszoke22@gmail.com>
2024-11-19 15:11:17 -0500

    class 5

--- fall24/AppSoftwareDev/wsu-test2/src/app/user/user.component.spec.ts
+++ fall24/AppSoftwareDev/wsu-test2/src/app/user/user.component.spec.ts
@@ -1,6 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { User } from '../../models/user.model';
+
+class MockUserService {
+  private User = [
+    {
+      id: '1',
+      name: 'John Doe',
+      email: 'john@doe.gov'
+    },
+    {
+      id: '2',
+      name: 'Jane Doe',
+      email: 'jane@doe.gov'
+    }
+  ]
+}
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -19,5 +35,18 @@ describe('UserComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('users initialized correctly', () => {
+    expect(component.users.length == 2).toBeTruthy();
+  });
+
+  it('correct user setup', () => {
+    expect(component.users.length == 3).toBeFalsy();
+    expect(component.users.includes({
+      id: 5,
+      name: 'John Secret',
+      email: 'madeup@aol.net'
+    })).toBeFalsy();
   });
 });
