f025144d24e6c0cdbc4939bb53a86bd55e54774a
caraz22 <cszoke22@gmail.com>
2025-01-17 13:10:21 -0500

    added comments

--- spring25/CS-1181/Sam/src/Dumb.java
+++ spring25/CS-1181/Sam/src/Dumb.java
@@ -6,26 +6,27 @@ public class Dumb {
 
         Scanner in = new Scanner(System.in);
 
-        int[] dumb = new int[10];
+        int[] dumb = new int[10]; // no need to make this 2 lines
 
+        // must prompt the user before starting the loop
         System.out.print("Pick a number between 1 and 100: ");
-        int userNum = in.nextInt();
+        int userNum = in.nextInt();     // no need to parse, you aren't taking from the array, you're just going to be adding to it
 
         for (int i = 0; i < dumb.length; i++) {
-            while (userNum < 1 || userNum > 100) {
-                System.out.print("Try again: ");
-                userNum = in.nextInt();
-            }
+            while (userNum < 1 || userNum > 100) {  // nested loop
+                System.out.print("Try again: ");  // invalid number? prompt for new one
+                userNum = in.nextInt();             // will repeat until valid number is entered
+            }                                       
 
             if (userNum > 0 && userNum < 101) {
-                dumb[i] = userNum;
+                dumb[i] = userNum;                  // valid number? set dumb at index i to the number
 
-                if (i == 9) {
-                    break;
+                if (i == 9) {           
+                    break;              // breaks out of the loop at the end so that you aren't prompted again once the array is full
                 }
 
-                System.out.print("Pick another number: ");
-                userNum = in.nextInt();
+                System.out.print("Pick another number: ");      // only reached if i < 9
+                userNum = in.nextInt();                           
             }
         }
 
