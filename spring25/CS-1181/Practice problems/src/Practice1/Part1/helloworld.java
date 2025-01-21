5bff1757adefb8e9dccb56b0aa4294d33c5b556e
caraz22 <cszoke22@gmail.com>
2025-01-21 15:53:30 -0500

    completed part 1 of practice problem and created file for part 2

---
+++ spring25/CS-1181/Practice problems/src/Practice1/Part1/helloworld.java
@@ -0,0 +1,9 @@
+package Part1;
+
+public class helloworld {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ spring25/CS-1181/Practice problems/src/Practice1/Part2/Main.java
@@ -0,0 +1,21 @@
+package Part2;
+public class Main {
+
+    public static void main(String[] args) {
+        
+        User user1 = new User("Michelle", "12345");
+        System.out.println("1. " + user1.isValidPassword()); // false -- less than 8 characters
+
+        User user2 = new User("Michelle", "12345Michelle");
+        System.out.println("2. " + user2.isValidPassword()); // false -- contains username
+
+        User user3 = new User("Michelle", "12345678");
+        System.out.println("3. " + user3.isValidPassword()); // true
+
+        System.out.println("4. " + user2.authenticate("ABCDE")); // false -- incorrect password
+        System.out.println("5. " + user2.authenticate("12345Michelle")); // true 
+
+        System.out.println("6. " + user3.authenticate("12345678")); // true
+    }
+
+}
